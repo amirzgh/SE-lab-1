37615e5b4f65216f26467dda871d4b9f550f0cc3
amirreza <amirreza.ghasemi@digikala.com>
2023-07-12 18:55:30 +0330

    feat-script: fix function naming

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function dis(val) {
     document.getElementById("result").value += val
 }
 
-function myFunction(event) {
+function initilize(event) {
     if (event.key == '0' || event.key == '1' 
         || event.key == '2' || event.key == '3'
         || event.key == '4' || event.key == '5' 
